fa46a8425a2872c249dcb419f7e13ef57f7a39db
Leonan Luppi <leonan.luppi@gmail.com>
2022-11-11 16:04:28 -0300

    v.1

---
+++ README.md
@@ -0,0 +1,13 @@
+# Skryva Landing Page Newsletter
+
+## Como usar?
+
+1 - Faça um clone do repositório.
+
+2 - Siga os comandos abaixo:
+```sh
+npm install
+npm run build
+```
+
+3 - Será gerado a pasta ```src/dist``` contendo todas as informações para enviar ao servidor.
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  }
+}
